f7a1675cc6de9f06e36c87f9cae6ffe876942a29
xiaolin <462441355@qq.com>
2021-09-11 14:03:55 +0800

    * 获取配置相关

---
+++ tpl/form.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'form-template',
+    templateName: '营销活动',
+    author: 'xiaolin',
+    snapshot: 'https://cdn.img.snapshot.png',
+    gitUrl: 'git@github.com:missxiaolin/form-cli.git',
+    projectType: '0'
+};
