576445baf3b0ec6326bb951a4d3898953c6aae95
xiaolin <462441355@qq.com>
2021-09-22 15:23:29 +0800

    * 生成模板

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const fs = require('fs')
 const program = require('commander');
 const initial = require('../command/initial');
+const generate = require('../command/generator');
 
 const pkg = require('../package.json')
 
@@ -9,11 +10,18 @@ const pkg = require('../package.json')
 let config = {};
 // 配置文件如果存在则读取
 if (fs.existsSync(path.resolve('form.config.js'))) {
-    config = require(path.resolve('form.config.js'));
+  config = require(path.resolve('form.config.js'));
 }
 
 program
-  .version(pkg.version,'-v, --version')
+  .version(pkg.version, '-v, --version')
   .command('init')
   .description('初始化 coco config 配置文件')
   .action(initial);
+
+program
+  .command('create [template]')
+  .description('生成 coco 模板')
+  .action(function (template) {
+    generate(template);
+  });
